883ad5b88faec7089c8b1b3fa028073fdcc2bf9a
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-17 18:24:32 -0300

    feat(CategoryController,routes): adiciona rotas para categorias

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const RecipeController = require('../controllers/RecipeController');
+const CategoryController = require('../controllers/CategoryController');
 
 router.get('/recipes', RecipeController.getAll);
 router.post('/recipes/create', RecipeController.create);
 router.put('/recipes/update/:id', RecipeController.update);
 router.delete('/recipes/destroy/:id', RecipeController.destroy);
+
+router.get('/categories', CategoryController.getAll);
+router.post('/categories/create', CategoryController.create);
+router.put('/categories/update/:id', CategoryController.update);
+router.delete('/categories/destroy/:id', CategoryController.destroy);
 
 module.exports = router;
